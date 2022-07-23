13b6a154efc6afe2e78c38300b6561b3ed41a770
Ben Churchill <ben.churchill@missionlabs.co.uk>
2022-07-23 21:52:59 +0100

    test

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
-import fastify from "fastify";
+import { randomBytes } from "crypto";
+import fastify, { FastifyRequest } from "fastify";
+import { MongoClient } from "mongodb";
 
 const server = fastify();
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb://mongo:ZdabZuYZ14fIKyxcL1F8@containers-us-west-72.railway.app:7612";
+const client = new MongoClient(uri);
 
-server.get("/", async (request, reply) => {
-  reply.code(200).send({ message: "Hello world!" });
+
+
+server.get("/", async (request: FastifyRequest, reply) => {
+  const response = await client.db('test').collection('users').insertOne({
+    name: 'test'
+  })
+
+  reply.code(200).send({ message: "Hello world!", response, });
 });
 
 server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
